531ccda85a076b232a86855a87b99186475dfe09
Kirti Tripathi <aryashiva622@gmail.com>
2025-12-15 12:42:22 +0530

    updated code

---
+++ firstProg.java
@@ -0,0 +1,11 @@
+public classs EvenOdd {
+    public static void main(String[] args){
+        int num = 7;
+        if(num%2==0){
+            Sytem.out.println(num + " is even");
+        }
+        else {
+             Sytem.out.println(num + " is odd");
+        }
+    }
+}
